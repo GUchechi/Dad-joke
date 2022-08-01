8058712f203954600975c2fcfc48903a8ff482e4
GUchechi <godswilluchendu7@gmail.com>
2022-08-01 21:22:24 +0100

    added async/await

--- scripts.js
+++ scripts.js
@@ -4,16 +4,34 @@ const jokeBtn = document.getElementById('jokeBtn')
 jokeBtn.addEventListener('click', generateJoke)
 generateJoke()
 
-function generateJoke() {
+// Using ASYNC/AWAIT
+async function generateJoke() {
     const config = {
         headers: {
             Accept: 'application/json',
         },
     }
 
-    fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke;
-    })  
+    const res = await fetch ('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    jokeEl.innerHTML = data.joke     
 }
+
+
+
+//  Using .then
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke;
+//     })  
+// }
